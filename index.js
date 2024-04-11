bf251c1b06bc54c1b19740c88cb46a3d49efa9c2
asathyjith <asathyajith98@gmail.com>
2024-04-11 18:36:33 +0530

    changes

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 //HTTP methods and status codes
 
-
-
-
-
-
-
 //SQL
 /*
 heap table 
 having 
 improving db performance 
 */
+
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+
+app.get('/test',(req, res)=>{
+
+	res.send("hello Node JS!");	
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
